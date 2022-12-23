fcebc5e073a796218fbad20b5a65227f381f7b10
Sergey Shishin <flyers32rus@gmail.com>
2022-12-23 19:50:13 +0300

    feat(mvp): release mvp ver1.0 with delete function and ui

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
+// Баг
+// При вводе примерно больше 5 заметок, последние не удаляются до тех пор, пока не удалить заметку номером до 5
+
 'use strict';
 
 const storageList = document.querySelector('.save-list'),
       button = document.querySelector('.note-form__btn'),
-      form = document.querySelector('.note-form__input');
+      form = document.querySelector('.note-form__input'),
+      resetBtn = document.querySelector('.note-form__btn-reset');
 
 let counter;
+let r = Object.keys(localStorage)
 
 if(localStorage.length < 1) {
     counter = 0;
 } else {
-    counter = localStorage.length;
+    counter = currentCount ()
 }
 
+function currentCount () {
+    let keys = Object.keys(localStorage);
+    
+    keys.sort((a,b) => {
+        return a - b;
+    });
+   return keys[keys.length - 1]
+} 
 
 function saveMessage() {
+    
+    form.onkeydown = (e) => {
+        if(e.keyCode === 13) {
+            e.preventDefault()
+            submitMessage();
+            deleteMessage()
+            form.value = ''
+        }
+    }
+
     button.addEventListener('click', () => {
-        const message = form.value;
+        submitMessage();
+        deleteMessage()
+    });
+    
+    
+}
+
+function submitMessage() {
+    const message = form.value;
         counter++;
         localStorage.setItem(counter, message);
         addMessage(message);
-        location.reload()
-    });
 }
 
 function addMessage(elem) {
@@ -37,11 +66,12 @@ function addMessage(elem) {
 
 function deleteMessage() {
     const elements = document.querySelectorAll('.save-list__item');
-   const valuesOfStorage = Object.values(localStorage);
    
     elements.forEach(item1 => {
         item1.addEventListener('dblclick', () => {
+            const valuesOfStorage = Object.values(localStorage);
             valuesOfStorage.forEach((item2, ind ) => {
+                item2 = item2.trim();
                 if(item1.innerText == item2) {
                    const u = Object.keys(localStorage)
                     localStorage.removeItem(u[ind])
@@ -55,7 +85,7 @@ function deleteMessage() {
 
 
 
-saveMessage();
+
 
 function render() {
     if(localStorage.length > 0) {
@@ -72,5 +102,14 @@ function render() {
     }
 }
 
+function resetStorage() {
+    resetBtn.addEventListener('click', () => {
+        localStorage.clear();
+        location.reload();
+    })
+}
+
+saveMessage();
 render();
 deleteMessage();
+resetStorage();
